0bdad13c81af1dfbe650e6ce00c9a0109e0d6737
enfycius <hexk0131@gmail.com>
2021-03-05 21:43:55 +0900

    fix Issue #1

---
+++ HBNU Task Scheduler/HBNU Task Scheduler/HBNU_Task_SchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HBNU_Task_SchedulerApp.swift
+//  HBNU Task Scheduler
+//
+//  Created by 김종혁 on 2021/03/05.
+//
+
+import SwiftUI
+
+@main
+struct HBNU_Task_SchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
