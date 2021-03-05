595ea9d7928aa43a485ea1f43372530f810dc21a
enfycius <hexk0131@gmail.com>
2021-03-06 00:06:34 +0900

    fix Issue #3

---
+++ HBNU Task Scheduler/HBNU Task Scheduler/KeyboardResponder.swift
@@ -0,0 +1,37 @@
+//
+//  KeyboardResponder.swift
+//  HBNU Task Scheduler
+//
+//  Created by 김종혁 on 2021/03/05.
+//
+
+import Foundation
+import SwiftUI
+
+class KeyboardResponder: ObservableObject {
+    @Published var currentHeight: CGFloat = 0
+    
+    var _center: NotificationCenter
+    
+    init(center: NotificationCenter = .default) {
+        _center = center
+        _center.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        _center.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyBoardWillShow(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            withAnimation {
+                currentHeight = keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyBoardWillHide(notification: Notification) {
+        withAnimation {
+            currentHeight = 0
+        }
+    }
+    
+    
+}
